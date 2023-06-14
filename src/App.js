3031d77d62e2019232207f9fbb3e3521092ab6ad
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-06-14 10:40:12 +0200

    add api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 import Counter from "./components/Counter";
 import "./styles/App.css";
 import Input from "./components/Input";
@@ -7,30 +7,38 @@ import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/Modal/MyModal";
 import MyButton from "./components/UI/Button/MyButton";
+import { usePosts } from "./hooks/usePosts";
+import axios from "axios";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "Javascript",
-      description: "Avascript is a program language",
-    },
-    {
-      id: 2,
-      title: "Avascript 2",
-      description: "Javascript is a program language",
-    },
-    {
-      id: 3,
-      title: "Script 3",
-      description: "Qavascript is a program language",
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
+    // {
+    //   id: 1,
+    //   title: "Javascript",
+    //   description: "Avascript is a program language",
+    // },
+    // {
+    //   id: 2,
+    //   title: "Avascript 2",
+    //   description: "Javascript is a program language",
+    // },
+    // {
+    //   id: 3,
+    //   title: "Script 3",
+    //   description: "Qavascript is a program language",
+    // },
+  
+
+  async function fetchPosts() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    setPosts(response.data);
+  }
 
   // const [selectedSort, setSelectedSort] = useState("");
   // const [searchQuery, setSearchQuery] = useState("");
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   // const sortedPosts = useMemo(() => {
   //   if (selectedSort) {
@@ -41,25 +49,25 @@ function App() {
   //   return posts;
   // }, [selectedSort, posts]);
 
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
+  // const sortedPosts = useMemo(() => {
+  //   if (filter.sort) {
+  //     return [...posts].sort((a, b) =>
+  //       a[filter.sort].localeCompare(b[filter.sort])
+  //     );
+  //   }
+  //   return posts;
+  // }, [filter.sort, posts]);
 
   // const sortedAndSearchedPosts = useMemo(() => {
   //   return sortedPosts.filter((post) =>
   //     post.title.toLowerCase().includes(searchQuery.toLowerCase())
   //   );
   // }, [searchQuery, sortedPosts]);
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query.toLowerCase())
-    );
-  }, [filter.query, sortedPosts]);
+  // const sortedAndSearchedPosts = useMemo(() => {
+  //   return sortedPosts.filter((post) =>
+  //     post.title.toLowerCase().includes(filter.query.toLowerCase())
+  //   );
+  // }, [filter.query, sortedPosts]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -77,6 +85,7 @@ function App() {
 
   return (
     <div className="App">
+      <button onClick={fetchPosts}>Get POSTS</button>
       <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
         Create POST
       </MyButton>
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PostItem from "./PostItem";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 const PostList = ({ posts, title, remove }) => {
   if (!posts.length) {
@@ -9,22 +10,30 @@ const PostList = ({ posts, title, remove }) => {
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>{title}</h1>
-      <PostItem
+      {/* <PostItem
         post={{
           id: 1,
           title: "Javascript",
           description: "Javascript is a program language",
         }}
-      />
-
-      {posts.map((post, index) => (
-        <PostItem
-          remove={remove}
-          number={index + 1}
-          post={post}
-          key={post.id}
-        />
-      ))}
+      /> */}
+      <TransitionGroup>
+        {posts.map((post, index) => 
+          <CSSTransition
+            key={post.id}
+           
+            timeout={500}
+            classNames="post"
+          >
+            <PostItem
+              remove={remove}
+              number={index + 1}
+              post={post}
+              key={post.id}
+            />
+          </CSSTransition>
+        )}
+      </TransitionGroup>
     </div>
   );
 };
